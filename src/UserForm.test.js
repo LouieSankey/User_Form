078a558d85c127466734b603f243cb9aed9bad29
Louis Sankey <lmsankey@ucdavis.edu>
2025-04-14 07:39:14 -0700

    first commit

---
+++ src/UserForm.test.js
@@ -0,0 +1,17 @@
+import {render, screen} from '@testing-library/react' 
+import user from '@testing-library/user-event'
+import UserForm from './UserForm'
+
+test('it shows two inputs and a button', () => {
+  //render component
+  render(<UserForm/>)
+
+  //manipulate component or find an element in it
+  const inputs = screen.getAllByRole('textbox')
+  const button = screen.getByRole('button')
+
+  expect(inputs).toHaveLength(2)
+  expect(button).toBeInTheDocument()
+
+
+})
